c7d39031c4e835f364414d304cdac1a682c86771
Ole Carlsen <ole.carlsen@yourstruly.de>
2020-03-09 15:58:38 +0100

    * resolve with error status, if response body can not get parsed as JSON

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function evaluateUrl(targetURL){
 				resolve(result);
 			} else {
 				if (response) {
-					console.log('\tyt-evaluate-url/index.js -> response=', response);
+					console.log('\tyt-evaluate-url/index.js -> response.body=', response.body);
 					// save status code of response
 					if (!!response.statusCode) {
 						result.status = response.statusCode;
@@ -53,8 +53,11 @@ function evaluateUrl(targetURL){
 						if (!!data.message) {
 							result.message = data.message;
 						}
-					} catch (e) {
-						console.log('Server-ERROR: \tyt-evaluate-url/index.js -> evaluateUrl(): cannot parse response.body as json.',e);
+					} catch (err) {
+						console.log('Server-ERROR: \tyt-evaluate-url/index.js -> evaluateUrl(): cannot parse response.body as json.', err);
+						result.status = 500;
+						result.message = 'Server-ERROR: \tyt-evaluate-url/index.js -> cannot parse response.body as json.' + error.toString();
+						resolve(result);
 					}
 					resolve(result);
 				} else {
